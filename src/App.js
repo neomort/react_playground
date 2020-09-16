666ce0a2fdaaab9165f5428e8fb0ba921c2257c6
neomort <kyle@kyleadams.com>
2020-09-16 17:10:03 -0600

    started

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container, Button } from 'semantic-ui-react';
+
+class App extends React.Component {
+  state = { 
+
+  };
+
+  render() {
+    
+  }
+
+}
+
+
+export default App;
